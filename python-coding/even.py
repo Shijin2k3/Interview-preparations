5c45cf600daa4b45ea5c98f69eabd8462e1c2135
Shijin2k3 <shijinkumar2003@gmail.com>
2025-05-08 10:48:29 +0530

    initialized  factorial and even no program

---
+++ python-coding/even.py
@@ -0,0 +1,6 @@
+n=int(input("Enter a number "))
+
+if(n % 2 == 0):
+    print("{} is a even number".format(n))
+else:
+    print("{} is a odd number".format(n))
---
+++ python-coding/factorial.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number "))
+def fact(n):
+    if(n ==0):
+        return 1
+    else:
+        result=1
+        for i in range(1,n+1):
+            result=result*i
+        print("factorial of {} is {}".format(n,result))
+
+fact(n)
+        
