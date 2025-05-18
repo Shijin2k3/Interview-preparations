18519341c9e7277dc63dff57a3ee9f1c0b4a7170
Shijin <138788615+Shijin2k3@users.noreply.github.com>
2025-05-18 18:16:09 +0530

    Update composite.py

--- python-coding/composite.py
+++ python-coding/composite.py
@@ -4,7 +4,7 @@ def composite(n):
     if(n==1 | n<1):
         isPrime=False
     for i in range(2,n-1):
-        if(n%i ==0)
+        if(n%i ==0):
             isPrime=False
     if(isPrime):
         print(" {} is Prime no".format(n))
